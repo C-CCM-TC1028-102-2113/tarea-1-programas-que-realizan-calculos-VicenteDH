f064aa13261cf4a963279702c2eaa859355dc187
VicenteDH <89059919+VicenteDH@users.noreply.github.com>
2021-08-21 17:29:56 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -2,7 +2,7 @@ def main():
     #escribe tu código abajo de esta línea
     m=float(input("Dame los minutos: " ))
     x= m*5.7*6
-    print("centimentros recorridos: "+str(x))
+    print("Centimentros recorridos: "+str(x))
     x= round(x,1)
     pass
 
