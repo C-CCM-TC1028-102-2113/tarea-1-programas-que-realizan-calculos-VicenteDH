87576a9556dcaaca54da6406043687a9968a3a28
VicenteDH <89059919+VicenteDH@users.noreply.github.com>
2021-08-21 22:44:00 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
+    palabras=int(input("Dame el número de palabras: "))
+    pag=palabras/475
+    total=pag*60
+    descuento=total*.1
+    total1=total-descuento
+    total1=round(total1,1)
+    print("El costo de la publicación es: "+str(total1))
     pass
-
 
 if __name__ == '__main__':
     main()
