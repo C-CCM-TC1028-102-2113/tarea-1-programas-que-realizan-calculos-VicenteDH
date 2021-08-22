126a94c455666f04889de2a734e4b0c745e589d6
VicenteDH <89059919+VicenteDH@users.noreply.github.com>
2021-08-22 17:16:09 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,9 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
     palabras=int(input("Dame el número de palabras: "))
-    pag=palabras/475
+    pag=(math.ceil(palabras/475))
     total=pag*60
-    descuento=total*.1
+    descuento=total*.10
     total1=total-descuento
     total1=round(total1,1)
     print("El costo de la publicación es: "+str(total1))
